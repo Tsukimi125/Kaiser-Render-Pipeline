ec6b52780d1b1c7641177815912195cf0782fa5b
Tsukimi <hrj1125@qq.com>
2024-01-23 12:19:42 +0800

    1

--- Assets/Kaiser RP/RendererFeatures/ScreenSpaceReflection.cs
+++ Assets/Kaiser RP/RendererFeatures/ScreenSpaceReflection.cs
@@ -4,7 +4,7 @@ using UnityEngine.Rendering.Universal;
 
 internal class ScreenSpaceReflection : ScriptableRendererFeature
 {
-    public Shader m_Shader;
+    private Shader m_Shader;
     public float m_Intensity;
 
     Material m_Material;
@@ -14,7 +14,11 @@ internal class ScreenSpaceReflection : ScriptableRendererFeature
     public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
     {
         if (renderingData.cameraData.cameraType == CameraType.Game)
+        {
+            m_RenderPass.Setup(renderingData);
             renderer.EnqueuePass(m_RenderPass);
+        }
+
     }
 
     public override void SetupRenderPasses(ScriptableRenderer renderer, in RenderingData renderingData)
@@ -31,6 +35,7 @@ internal class ScreenSpaceReflection : ScriptableRendererFeature
 
     public override void Create()
     {
+        m_Shader = Shader.Find("Hidden/KaiserRP/ScreenSpaceReflection");
         m_Material = CoreUtils.CreateEngineMaterial(m_Shader);
         m_RenderPass = new ScreenSpaceReflectionPass(m_Material);
     }
@@ -46,11 +51,19 @@ internal class ScreenSpaceReflection : ScriptableRendererFeature
         Material m_Material;
         RTHandle m_CameraColorTarget;
         float m_Intensity;
+        RTHandle m_CopiedColor;
 
         public ScreenSpaceReflectionPass(Material material)
         {
             m_Material = material;
             renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
+        }
+
+        public void Setup(in RenderingData renderingData)
+        {
+            var colorCopyDescriptor = renderingData.cameraData.cameraTargetDescriptor;
+            colorCopyDescriptor.depthBufferBits = (int)DepthBits.None;
+            RenderingUtils.ReAllocateIfNeeded(ref m_CopiedColor, colorCopyDescriptor, name: "_FullscreenPassColorCopy");
         }
 
         public void SetTarget(RTHandle colorHandle, float intensity)
@@ -67,6 +80,9 @@ internal class ScreenSpaceReflection : ScriptableRendererFeature
         public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
         {
             var cameraData = renderingData.cameraData;
+            var source = renderingData.cameraData.renderer.cameraColorTargetHandle;
+            Vector2 viewportScale = source.useScaling ? new Vector2(source.rtHandleProperties.rtHandleScale.x, source.rtHandleProperties.rtHandleScale.y) : Vector2.one;
+            Debug.Log(viewportScale);
             if (cameraData.camera.cameraType != CameraType.Game)
                 return;
 
@@ -74,10 +90,21 @@ internal class ScreenSpaceReflection : ScriptableRendererFeature
                 return;
 
             CommandBuffer cmd = CommandBufferPool.Get();
+
+
+
             using (new ProfilingScope(cmd, m_ProfilingSampler))
             {
+
+                Blitter.BlitCameraTexture(cmd, source, m_CopiedColor);
                 m_Material.SetFloat("_Intensity", m_Intensity);
-                Blitter.BlitCameraTexture(cmd, m_CameraColorTarget, m_CameraColorTarget, m_Material, 0);
+                m_Material.SetTexture("_SSR_ColorTexture", m_CopiedColor);
+                // Blitter.BlitCameraTexture(cmd, source, m_CameraColorTarget, m_Material, 0);
+
+                CoreUtils.SetRenderTarget(cmd, cameraData.renderer.cameraColorTargetHandle);
+                CoreUtils.DrawFullScreen(cmd, m_Material);
+                // context.ExecuteCommandBuffer(cmd);
+                // cmd.Clear();
             }
             context.ExecuteCommandBuffer(cmd);
             cmd.Clear();
--- Assets/Kaiser RP/Resources/Shaders/ScreenSpaceReflection.shader
+++ Assets/Kaiser RP/Resources/Shaders/ScreenSpaceReflection.shader
@@ -1,4 +1,4 @@
-Shader "ColorBlit"
+Shader "Hidden/KaiserRP/ScreenSpaceReflection"
 {
     SubShader
     {
@@ -7,7 +7,7 @@ Shader "ColorBlit"
         ZWrite Off Cull Off
         Pass
         {
-            Name "ColorBlitPass"
+            Name "ScreenSpaceReflectionPass"
 
             HLSLPROGRAM
             #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
@@ -21,13 +21,22 @@ Shader "ColorBlit"
             TEXTURE2D_X(_CameraOpaqueTexture);
             SAMPLER(sampler_CameraOpaqueTexture);
 
+            TEXTURE2D_X(_SSR_ColorTexture);
+            SAMPLER(sampler_SSR_ColorTexture);
+
+            TEXTURE2D(_MainTex);
+            SAMPLER(sampler_MainTex);
+
             float _Intensity;
 
             half4 frag(Varyings input):SV_Target
             {
                 UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX(input);
-                float4 color = SAMPLE_TEXTURE2D_X(_CameraOpaqueTexture, sampler_CameraOpaqueTexture, input.texcoord);
-                return color * float4(0, _Intensity, 0, 1);
+                float2 uv = input.positionCS.xy / float2(1920, 1080);
+                float4 color = _SSR_ColorTexture.SampleLevel(sampler_SSR_ColorTexture, uv, 0);
+                return color * float4(1, 1, 1, 1);
+                // return float4(uv, 0, 1);
+
             }
             ENDHLSL
         }
